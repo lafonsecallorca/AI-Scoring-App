ef024846ea9dd2338ee33087959d35ebfb07cce7
lafonsecallorca <127278198+lafonsecallorca@users.noreply.github.com>
2024-02-29 21:01:59 -0500

    Created streamlit interface

--- app.py
+++ app.py
@@ -5,14 +5,26 @@ import os
 
 load_dotenv() #this will load all env variables
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 prompt="""You are a text summarizer. You will be taking the text provided
-and summarizing  the entire text in key points within 300 words. The text will be
-appended here : """
+and summarizing  the entire text in key points within 300 words. Please provide the 
+summary of the text given here : """
 
 def generate_summary(original_text, prompt):
     model=genai.GenerativeModel("gemini-pro")
 
     response=model.generate_content(prompt+original_text)
     return response.text
+
+st.title("Text Summarizer")
+user_text_input = st.text_input("Enter the text you would like to summarize:")
+
+if st.button("Get Summary"):
+    summary = generate_summary(user_text_input, prompt)
+    st.markdown("AI Summary:")
+    st.write(summary)
+
+
+
+
